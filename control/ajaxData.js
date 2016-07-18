d3643f420a77be7a9b2872798a6a6cd4bb66a9eb
wsscat <2587575267@qq.com>
2016-07-19 07:12:01 +0800

    小型但页面应用

---
+++ control/ajaxData.js
@@ -0,0 +1,27 @@
+//数据
+var newsList = [{
+	id: 0,
+	title: '李同学',
+	content: 'Hi',
+	date: new Date()
+}, {
+	id: 1,
+	title: '姚同学',
+	content: 'Hello Word',
+	date: new Date()
+}, {
+	id: 2,
+	title: '陈同学',
+	content: 'Nice',
+	date: new Date()
+}, {
+	id: 3,
+	title: '赖同学',
+	content: '你好',
+	date: new Date()
+}, {
+	id: 4,
+	title: '吴同学',
+	content: '',
+	date: new Date()
+}];
